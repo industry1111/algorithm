f23f862bf168f082ce381a7fc66faf9ca8714f96
industry1111@naver.com <industry1111@naver.com>
2022-11-01 00:11:26 +0900

    11월1일 Stack 개념 이해하기 stack.java 40~47 개선하기

--- src/main/java/programmers/leveltwo/stack.java
+++ src/main/java/programmers/leveltwo/stack.java
@@ -1,6 +1,7 @@
 package programmers.leveltwo;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 class stack {
     boolean solution(String s) {
@@ -25,4 +26,31 @@ class stack {
         return answer;
     }
     //stack을 사용해서 풀어보기!!
+     /*
+       Stack<E>
+       empty()  Tests if this stack is empty.
+       push(E item)
+       pop() return E
+    */
+    boolean solution2(String s) {
+        boolean answer = true;
+        Stack<Integer> st = new Stack<>();
+        char[] arr = s.toCharArray();
+
+        for (int i=0; i<arr.length; i++) {
+            if ( arr[i] == '(') {
+                st.push(0);
+            } else {
+                if (!st.empty() && st.pop() == 0){
+                    answer = true;
+                } else {
+                    answer = false;
+                    break;
+                }
+            }
+
+        }
+        if (!st.empty()) answer = false;
+        return answer;
+    }
 }
