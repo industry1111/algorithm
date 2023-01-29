c76050103df051af35d90f5dab3e3c3f4854dde8
Travel <98158673+industry1111@users.noreply.github.com>
2023-01-29 15:25:53 +0900

    DFS 깊이우선탐색 알고리즘 -재귀함수에 대한 개념  -1. 수행동작   2. 탈출조건  - 자바 static, 접근제어자

---
+++ src/main/java/programmers/leveltwo/countIsland.java
@@ -0,0 +1,104 @@
+package programmers.leveltwo;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/*
+* 메리는 여름을 맞아 무인도로 여행을 가기 위해 지도를 보고 있습니다. 지도에는 바다와 무인도들에 대한 정보가 표시돼 있습니다.
+*  지도는 1 x 1크기의 사각형들로 이루어진 직사각형 격자 형태이며, 격자의 각 칸에는 'X' 또는 1에서 9 사이의 자연수가 적혀있습니다.
+* 지도의 'X'는 바다를 나타내며, 숫자는 무인도를 나타냅니다. 이때, 상, 하, 좌, 우로 연결되는 땅들은 하나의 무인도를 이룹니다.
+* 지도의 각 칸에 적힌 숫자는 식량을 나타내는데, 상, 하, 좌, 우로 연결되는 칸에 적힌 숫자를 모두 합한 값은
+* 해당 무인도에서 최대 며칠동안 머물 수 있는지를 나타냅니다.
+* 어떤 섬으로 놀러 갈지 못 정한 메리는 우선 각 섬에서 최대 며칠씩 머물 수 있는지 알아본 후 놀러갈 섬을 결정하려 합니다.
+* 지도를 나타내는 문자열 배열 maps가 매개변수로 주어질 때,
+* 각 섬에서 최대 며칠씩 머무를 수 있는지 배열에 오름차순으로 담아 return 하는 solution 함수를 완성해주세요.
+*  만약 지낼 수 있는 무인도가 없다면 -1을 배열에 담아 return 해주세요.
+*
+*
+* Input (maps)	                        return (result)
+* ["X591X","X1X5X","X231X", "1XXX1"]	[1, 1, 27]
+* ["XXX","XXX","XXX"]	                   [-1]
+* */
+public class countIsland {
+    static int col;
+    static int row;
+    static String[] arr;
+    static int result;
+
+    private static boolean dfs(int x, int y,int sum) {
+
+        if ( x < 0 || x >= row || y < 0 || y >= col || arr[x].charAt(y) == 'X' ) return false;
+
+            StringBuilder str = new StringBuilder(arr[x]);
+            if (arr[x].charAt(y) != 'Y' ) {
+
+                result += arr[x].charAt(y)-48;
+                str.setCharAt(y, 'Y');
+                arr[x] = String.valueOf(str);
+
+                dfs(x, y - 1,result);
+                dfs(x - 1, y,result);
+                dfs(x, y + 1,result);
+                dfs(x + 1, y,result);
+
+                return true;
+            }
+
+            return false;
+    }
+
+    public static void main(String[] args) {
+        arr = new String[] {"5X2X39","9XXX32","X9X33X", "X4X93X"};
+        col = arr[0].length();
+        row = arr.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        int sum = 0;
+        for (int i=0; i<row;i++) {
+            for (int j=0; j<col; j++) {
+                result = 0;
+                if (dfs(i,j,result)) {
+                    list.add(result);
+                }
+            }
+        }
+        int idx = 0;
+
+        int[] intArr = new int[list.size()];
+        for (Integer integer : list) {
+            intArr[idx++] = integer;
+        }
+        System.out.println(Arrays.toString(intArr));
+        sort(intArr);
+    }
+
+    private static void sort (int[] arr) {
+
+        int tmp = 0;
+        int min = 0;
+//        for (int i=0; i<arr.length; i++) {
+//            for ( int j=i+1; j<arr.length; j++) {
+//                if ( arr[i] > arr[j]) {
+//                    tmp = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = tmp;
+//                }
+//            }
+//        }
+        for (int i=0; i<arr.length-1; i++) {
+            min = i;
+            for ( int j=i+1; j<arr.length; j++) {
+                if ( arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+}
+
+
---
+++ src/main/java/programmers/leveltwo/targetNumber.java
@@ -0,0 +1,48 @@
+package programmers.leveltwo;
+/*
+n개의 음이 아닌 정수들이 있습니다. 이 정수들을 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다.
+ 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+사용할 수 있는 숫자가 담긴 배열 numbers,타겟 넘버 target이 매개변수로
+주어질 때 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는
+방법의 수를 return 하도록 solution 함수를 작성해주세요.
+*/
+public class targetNumber {
+
+    static int[] numbers;
+    static int target;
+    static int result;
+
+    private static void dfs(int index, int sum) {
+        //2. 탈출조건
+        if (numbers.length == index) {
+            if(sum == target) result++;
+            return;
+        }
+        //1. 수행동작
+        dfs(index+1,sum+numbers[index]);
+        dfs(index+1,sum-numbers[index]);
+    }
+
+    private static void solution(int[] numbers, int target){
+        targetNumber.numbers = numbers;
+        targetNumber.target = target;
+        result = 0;
+
+        dfs(0,0);
+
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        numbers = new int[]{1,1,1,1,1};
+        target = 3;
+
+        solution(numbers,target);
+    }
+}
